88e8fc2ac723224134baffab9d72c47aaf7b43e4
Riley Robertson <jrr8@rice.edu>
2020-04-07 18:18:17 -0400

    Update User model constructor

--- src/models/User.js
+++ src/models/User.js
@@ -8,28 +8,13 @@ export default class User {
   itemsInInventory;
 
   constructor(data) {
-    if(Array.isArray(data.hideoutModulesCompleted)){
-      this.hideoutModulesCompleted = new Map(data.hideoutModulesCompleted);
-    }else{
-      this.hideoutModulesCompleted = data.hideoutModulesCompleted;
+    ['hideoutModulesCompleted', 'trackedModules',
+      'itemsOwned', 'itemsInInventory'].forEach(getMapFromMapOrArray)
+
+    function getMapFromMapOrArray(property) {
+      const mapOrArray = data[property];
+      this[property] = Array.isArray(mapOrArray) ? new Map(mapOrArray) : mapOrArray;
     }
 
-    if(Array.isArray(data.trackedModules)){
-      this.trackedModules = new Map(data.trackedModules);
-    }else{
-      this.trackedModules = data.trackedModules;
-    }
-
-    if(Array.isArray(data.itemsOwned)){
-      this.itemsOwned = new Map(data.itemsOwned);
-    }else{
-      this.itemsOwned = data.itemsOwned;
-    }
-
-    if(Array.isArray(data.itemsInInventory)){
-      this.itemsInInventory = new Map(data.itemsInInventory);
-    }else{
-      this.itemsInInventory = data.itemsInInventory;
-    }
   }
 }
